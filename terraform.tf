55902756601c8886be48a64b42ee96df6f04de5e
Aakarsh-Sinha-Pro <Aakarsh.Sinha@cognizant.com>
2026-06-19 13:43:15 +0530

    Removed pessimistic constraint operator

--- terraform.tf
+++ terraform.tf
@@ -12,25 +12,25 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0.0"
+      version = ">= 6.42.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.1"
+      version = ">= 3.6.1"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.5"
+      version = ">= 4.0.5"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.4"
+      version = ">= 2.3.4"
     }
   }
 
-  required_version = "~> 1.3"
+  required_version = ">= 1.3"
 }
 
