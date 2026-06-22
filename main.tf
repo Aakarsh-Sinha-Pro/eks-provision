df8e99d02ba8f2a269e0ce2c75a5e6fa6d611230
Aakarsh-Sinha-Pro <Aakarsh.Sinha@cognizant.com>
2026-06-22 17:47:40 +0530

    Setting before_compute as true

--- main.tf
+++ main.tf
@@ -281,12 +281,112 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+# provider "aws" {
+#   region = var.region
+# }
+
+# locals {
+#   cluster_name = "eks-aakarsh-cluster-v10"
+# }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 21.0"
+
+#   name               = local.cluster_name
+#   kubernetes_version = "1.35"
+
+#   create_iam_role = false  
+#   iam_role_arn    = "arn:aws:iam::691879165105:role/CCL-EKS-Role" 
+
+#   create_node_security_group = true
+#   create_security_group      = true 
+
+#   enable_irsa            = false
+#   endpoint_public_access  = true
+#   endpoint_private_access = true 
+
+#   enable_cluster_creator_admin_permissions = false
+  
+#   # FIXED: Set to the validated string to satisfy the Terraform validation engine
+#   authentication_mode = "API_AND_CONFIG_MAP"
+
+#   # Structural rule to automatically map port 443 for your local network jumpbox
+#   security_group_additional_rules = {
+#     ingress_ec2_jumpbox = {
+#       description = "Allow administrative jumpbox subnet to reach EKS API Control Plane"
+#       protocol    = "tcp"
+#       from_port   = 443
+#       to_port     = 443
+#       type        = "ingress"
+#       cidr_blocks = ["172.31.0.0/16"] 
+#     }
+#   }
+
+#   access_entries = {
+#     admin_user = {
+#       principal_arn     = "arn:aws:iam::691879165105:user/2492176"
+#       type              = "STANDARD"
+#       policy_associations = {
+#         admin = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy" 
+#           access_scope = {
+#             type = "cluster"
+#           }
+#         }
+#       }
+#     }
+#     worker_nodes = {
+#       principal_arn = "arn:aws:iam::691879165105:role/CCL-EKS-NodeRole"
+#       type          = "EC2_LINUX"
+#     }
+#   }
+
+#   vpc_id     = "vpc-3af53251"
+#   subnet_ids = ["subnet-03426cca90ce6d80b", "subnet-003f5e1c0abc2ff75", "subnet-0ec2990e4c42fb65e"]
+
+#   eks_managed_node_groups = {
+#     one = {
+#       name = "node-group-1"
+
+#       instance_types = ["t3.small"]
+
+#       min_size     = 1
+#       max_size     = 3
+#       desired_size = 1
+
+#       ami_type        = "AL2023_x86_64_STANDARD"
+#       create_iam_role = false 
+#       iam_role_arn    = "arn:aws:iam::691879165105:role/CCL-EKS-NodeRole" 
+#     }
+#   }
+# }
+
+# # Isolated addon resources with explicit creation dependencies
+# resource "aws_eks_addon" "addons" {
+#   for_each = toset(["vpc-cni", "kube-proxy", "coredns"])
+
+#   cluster_name                = module.eks.cluster_name
+#   addon_name                  = each.key
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+
+#   # Forces addons to wait until node groups settle to prevent initialization hangs
+#   depends_on = [module.eks.eks_managed_node_groups]
+# }
+
+
+
+
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 provider "aws" {
   region = var.region
 }
 
 locals {
-  cluster_name = "eks-aakarsh-cluster-v10"
+  cluster_name = "eks-aakarsh-cluster-v7"
 }
 
 module "eks" {
@@ -307,9 +407,7 @@ module "eks" {
   endpoint_private_access = true 
 
   enable_cluster_creator_admin_permissions = false
-  
-  # FIXED: Set to the validated string to satisfy the Terraform validation engine
-  authentication_mode = "API_AND_CONFIG_MAP"
+  authentication_mode                      = "API_AND_CONFIG_MAP"
 
   # Structural rule to automatically map port 443 for your local network jumpbox
   security_group_additional_rules = {
@@ -345,6 +443,26 @@ module "eks" {
   vpc_id     = "vpc-3af53251"
   subnet_ids = ["subnet-03426cca90ce6d80b", "subnet-003f5e1c0abc2ff75", "subnet-0ec2990e4c42fb65e"]
 
+  # FIXED Lifecycle Mapping: Correct variable name for v21
+  addons = {
+    vpc-cni = {
+      before_compute              = true # Crucial: Fixes the initialization catch-22
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
+    }
+    kube-proxy = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
+    }
+    coredns = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
+    }
+  }
+
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
@@ -361,16 +479,3 @@ module "eks" {
     }
   }
 }
-
-# # Isolated addon resources with explicit creation dependencies
-# resource "aws_eks_addon" "addons" {
-#   for_each = toset(["vpc-cni", "kube-proxy", "coredns"])
-
-#   cluster_name                = module.eks.cluster_name
-#   addon_name                  = each.key
-#   resolve_conflicts_on_create = "OVERWRITE"
-#   resolve_conflicts_on_update = "OVERWRITE"
-
-#   # Forces addons to wait until node groups settle to prevent initialization hangs
-#   depends_on = [module.eks.eks_managed_node_groups]
-# }
